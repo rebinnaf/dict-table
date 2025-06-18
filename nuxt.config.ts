c59af449859217977c07532a4a25d514d6e6a428
Nadia Moradi <rebinnaf@gmail.com>
2025-06-18 11:27:55 +0200

    chore: make ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import Aura from '@primeuix/themes/aura'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   nitro: {
